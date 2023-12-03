ad3dbf00df65909263225449e0e206aa07cc6fa7
Danya Kogan <75654428+dreginkogan@users.noreply.github.com>
2023-12-02 19:20:40 -0500

    Add files via upload

--- Apps.py
+++ Apps.py
@@ -16,8 +16,78 @@ class TicTacToe(App):
         self.macropad = macropad
         self.name = "tictactoe"
 
+    def print_board(self, backend):
+        macropad = self.macropad
+        text_lines = macropad.display_text(title="==== Tic Tac Toe ====")
+
+        BOARD_DICT = {
+            -1: "-",
+            0: "X",
+            1: "O",
+        }
+
+        print(backend[0][0])
+
+        for i in range(3):
+            text_lines[i].text = BOARD_DICT[backend[i][0]] + BOARD_DICT[backend[i][1]] + BOARD_DICT[backend[i][2]]
+
+        text_lines.show()
+
     def run(self):
-        print("does nothing rn")
+
+        macropad = self.macropad
+
+        curr_board = [
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1],
+        ]
+
+        self.print_board(curr_board)
+
+        curr_player = 0  # translate via BOARD_DICT
+        button_held = False
+
+        while True:
+            if macropad.encoder_switch == 1:
+                break
+
+            key_event = macropad.keys.events.get()
+            if key_event:
+                if key_event.pressed:
+                    if not button_held:
+                        button_held = True
+                        user_input: int = key_event.key_number # 0 to 11 inclusive
+
+                        if user_input == 11: # quit
+                            print("joe mama")
+
+                        elif user_input == 10: # redo
+                            curr_board = [
+                                [-1, -1, -1],
+                                [-1, -1, -1],
+                                [-1, -1, -1],
+                            ]
+                            curr_player = 0
+                            self.print_board(curr_board)
+                            continue
+
+                        elif 0 <= user_input < 9:
+                            row = user_input // 3
+                            col = user_input % 3
+
+                            curr_board[row][col] = curr_player
+                            self.print_board(curr_board)
+
+                            curr_player = 1 if curr_player == 0 else 0
+
+                        else:
+                            continue
+
+                    else: # if button is held,
+                        pass # do nothing
+                else: # no key is pressed
+                    button_held = False
 
 
 class Sussy(App):
@@ -64,17 +134,19 @@ class Snake(App):
         print("also also does nothing rn")
 
 
-class Tester(Program):
+class Tester(App):
     def __init__(self, macropad):
         self.macropad = macropad
         self.name = "tester"
 
     def run(self):
-        macropad = self.macropad 
-        
+        macropad = self.macropad
+
         text_lines = macropad.display_text(title="======= test =======")
+        text_lines.show()
         text_lines[0].text = "test app"
         text_lines[1].text = "will close"
         text_lines[2].text = "in 2s"
-        text_lines.show()
-        time.sleep(2)
+        time.sleep(1)
+        text_lines[2].text = "in 1s"
+        time.sleep(1)
